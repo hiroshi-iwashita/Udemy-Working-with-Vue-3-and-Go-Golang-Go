6bcc5559d36d7d81fbb20941720b8f7afae519a8
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-10 07:12:07 +0900

    connecting db in main func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,18 +5,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/hiroshi-iwashita/Udemy-Working-with-Vue-3-and-Go-Golang-Go/internal/driver"
 )
 
+// config is the type for all application configuration
 type config struct {
-	port int
+	port int //what port do we want the web server to listen on
 }
 
+// application is the type for all data we want to share with the
+// various parts of our application. We will share this information
+// in most cases by using this type as the receiver for functions.
 type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	db       *driver.DB
 }
 
+// main is the main entry point for our application
 func main() {
 	var cfg config
 	cfg.port = 8081
@@ -24,13 +32,20 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	dsn := "host=localhost port=5432 user=postgres password=password dbname=vueapi sslmode=disable timezone=UTC connect_timeout=5"
+	db, err := driver.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("cannot connect to database")
+	}
+
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		db:       db,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		log.Fatal(err)
 	}
