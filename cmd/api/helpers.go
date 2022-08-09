3c2b7e951654955d593a04196c0db5a4a5aa5ade
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-09 18:54:37 +0900

    import io

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"io"
 	"net/http"
 )
 
-func (app *application) readJSON(w http.ResponseWriter r *http.Request data interface{}) error {
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1048576 // one megabyte
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes)) 
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(data)
@@ -17,7 +18,7 @@ func (app *application) readJSON(w http.ResponseWriter r *http.Request data inte
 	}
 
 	err = dec.Decode(&struct{}{})
-	if err != nil {
+	if err != io.EOF {
 		return errors.New("body must jave only a single json value")
 	}
 
@@ -42,6 +43,6 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	if err != nil {
 		return err
 	}
-	
+
 	return nil
 }
