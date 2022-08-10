70ceb5f3fab72e1684c2c84ba460d76f672beecd
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-11 07:38:15 +0900

    add envelop so that get easy to read JSON

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -8,8 +8,10 @@ import (
 type jsonResponse struct {
 	Error   bool        `json:"error"`
 	Message string      `json:"message"`
-	Data    interface{} `jsonL:data`
+	Data    interface{} `json:"data,omitempty"`
 }
+
+type envelope map[string]interface{}
 
 // Login is the handler used to attempt to log a user into the api
 func (app *application) Login(w http.ResponseWriter, r *http.Request) {
