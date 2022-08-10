903828e34e12242f6efb0800f2d7a93c04f50b80
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-11 07:28:33 +0900

    add routes for Token

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -74,6 +75,73 @@ func (app *application) routes() http.Handler {
 		app.infoLog.Println("Got back id of", id)
 		newUser, _ := app.models.User.GetOne(id)
 		app.writeJSON(w, http.StatusOK, newUser)
+	})
+
+	mux.Get("/test-generate-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(2, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.Email = "admin@example.com"
+		token.CreatedAt = time.Now()
+		token.UpdatedAt = time.Now()
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+	})
+
+	mux.Get("/test-save-token", func(w http.ResponseWriter, r *http.Request) {
+		token, err := app.models.User.Token.GenerateToken(2, 60*time.Minute)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		user, err := app.models.User.GetOne(2)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		token.UserID = user.ID
+		token.CreatedAt = time.Now()
+		token.UpdatedAt = time.Now()
+
+		err = token.Insert(*token, *user)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		payload := jsonResponse{
+			Error:   false,
+			Message: "success",
+			Data:    token,
+		}
+
+		app.writeJSON(w, http.StatusOK, payload)
+	})
+
+	mux.Get("/test-validate-token", func(w http.ResponseWriter, r *http.Request) {
+		tokenToValidate := r.URL.Query().Get("token")
+		valid, err := app.models.Token.ValidToken(tokenToValidate)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		var payload jsonResponse
+		payload.Error = false
+		payload.Data = valid
+
+		app.writeJSON(w, http.StatusOK, payload)
 	})
 
 	return mux
